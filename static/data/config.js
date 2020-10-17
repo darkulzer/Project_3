7d828abab0980ccd0773b9cb5a0bc54c9deb8234
darkulzer <65078183+darkulzer@users.noreply.github.com>
2020-10-17 11:28:04 -0400

    html/css,Jupyter notebook,cleaned data

---
+++ static/data/config.js
@@ -0,0 +1,7 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYjNhbnMiLCJhIjoiY2tla2szMG1zMDVpODJybjYxem93c3JveSJ9.uyCNY3wnBC9Xu3cApmVlmg";
+
+var api_nash = "7fn2kqmzrpitqvfun958tdrql"
+
+//secret key
+//2xeh3o1f00huss27gkexf48nmqd16w8pp0oiai2v51v6e06rfw
